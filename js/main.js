9424f7aec39e5757176f81b906375fa0492c1715
MISSIC <prajjwalt9@gmail.com>
2022-04-01 22:40:57 +0545

    addint the buttons

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", () => {
     let nav = document.querySelector('nav');
     nav.classList.toggle('sticky', window.scrollY > 35);
 })
+let buttonsWrapper = document.querySelector('.buttons--wrapper');
+buttonsWrapper.addEventListener('scroll', (e) => {
+    if (buttonsWrapper.scrollLeft > buttonsWrapper.clientWidth * 0.75) {
+        document.querySelector('.button--section--hover').classList.replace('button--right', 'button--left');
+    } else if(buttonsWrapper.scrollLeft < buttonsWrapper.clientWidth * 0.75) {
+        document.querySelector('.button--section--hover').classList.replace('button--left', 'button--right');
+    }
+})
