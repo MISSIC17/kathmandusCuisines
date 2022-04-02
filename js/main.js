cde86f9bcdfd772fcaa198901be1f0444618a286
MISSIC <prajjwalt9@gmail.com>
2022-04-02 12:39:06 +0545

    responsive fix

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let buttonsWrapper = document.querySelector('.buttons--wrapper');
 buttonsWrapper.addEventListener('scroll', (e) => {
     if (buttonsWrapper.scrollLeft > buttonsWrapper.clientWidth * 0.75) {
         document.querySelector('.button--section--hover').classList.replace('button--right', 'button--left');
-    } else if(buttonsWrapper.scrollLeft < buttonsWrapper.clientWidth * 0.75) {
+    } else if (buttonsWrapper.scrollLeft < buttonsWrapper.clientWidth * 0.75) {
         document.querySelector('.button--section--hover').classList.replace('button--left', 'button--right');
     }
 })
